60fbc921e9a9d23a41642265c33c9b1ee74d013f
Kayo Ronald <kayoronald4321@gmail.com>
2023-08-25 16:54:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var cpf = "111.444.777-05".replace(/[^\d]/g, "");
-var arrayCbf =Array.from(cpf, (x) => parseInt(x))
+document.getElementById("click").addEventListener("click", () => {
+var valor = document.getElementById("cpf").value
+var cpf = valor.trim().replace(/[^\d]/g, "");
+var arrayCbf = Array.from(cpf, (x) => parseInt(x))
 
 var sum = 0
 for(let i= 0;i < 9; i++){
   sum += arrayCbf[i] * (10-i)
 }
-var t = (sum*10)%11
-console.log(sum)
+var resto = sum%11
+Swal.fire(String(resto))
+})
