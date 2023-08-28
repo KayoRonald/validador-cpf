f098f4b1b771cd8543620f97c6d10acddc8d2778
Kayo Ronald <kayoronald4321@gmail.com>
2023-08-28 18:51:32 +0000

    feat: update

--- main.go
+++ main.go
@@ -14,31 +14,21 @@ func main() {
 	// Or from an embedded system
 	// See github.com/gofiber/embed for examples
 	// engine := html.NewFileSystem(http.Dir("./views", ".hbs"))
-
 	// Pass the engine to the Views
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
-
 	app.Get("/", func(c *fiber.Ctx) error {
 		// Render index
-		return c.Render("teste", fiber.Map{
+		return c.Render("validatecpf", fiber.Map{
 			"Title": "Validar CPF",
 		}, "layouts/index")
 	})
-	app.Get("/oi", func(c *fiber.Ctx) error {
-		// Render index
+  app.Use(func(c *fiber.Ctx) error {
 		return c.Render("error", fiber.Map{
-			"Title": "Validar CPF",
+			"Title": "Página não encontrada",
 		}, "layouts/index")
 	})
-
-	// app.All("*", func(c *fiber.Ctx) error {
-	// 	// Render index within layouts/main
-	// 	return c.Render("error", fiber.Map{
-	// 		"Title": "Página não encontrada",
-	// 	}, "layouts/index")
-	// })
 
 	log.Fatal(app.Listen(":3000"))
 }
