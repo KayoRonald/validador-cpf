cf78bf8ae879706f905965b4bd798f8aa75d9e2b
Kayo Ronald <kayoronald4321@gmail.com>
2023-08-29 13:15:16 +0000

    feat: update

--- main.go
+++ main.go
@@ -2,24 +2,18 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/handlebars/v2"
 )
 
 func main() {
-	// Create a new engine
 	engine := handlebars.New("./views", ".hbs")
-
-	// Or from an embedded system
-	// See github.com/gofiber/embed for examples
-	// engine := html.NewFileSystem(http.Dir("./views", ".hbs"))
-	// Pass the engine to the Views
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
 	app.Get("/", func(c *fiber.Ctx) error {
-		// Render index
 		return c.Render("validatecpf", fiber.Map{
 			"Title": "Validar CPF",
 		}, "layouts/index")
@@ -29,6 +23,10 @@ func main() {
 			"Title": "Página não encontrada",
 		}, "layouts/index")
 	})
-
-	log.Fatal(app.Listen(":3000"))
+	port := os.Getenv("port")
+	if port == "" {
+		log.Print("Environment specified PORT, '3000' otherwise")
+		port = "3000"
+	}
+	app.Listen(":" + port)
 }
