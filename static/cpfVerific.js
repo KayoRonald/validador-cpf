c24ca236ab5bcd9023cb9e591cd3ee4c9f41ad59
Kayo Ronald <kayoronald4321@gmail.com>
2023-08-30 11:31:54 +0000

    feat: create script valid cpf

---
+++ static/cpfVerific.js
@@ -0,0 +1,21 @@
+const verificCPF = (arrayCPF, cpf) => {
+  let base = cpf.slice(0, 9);
+  let sum1 = 0
+  for (let i = 0; i < 9; i++) {
+    sum1 += arrayCbf[i] * (10 - i)
+  }
+  const resto1 = sum1 % 11
+  let digite1 = resto1 < 2 ? 0 : 11 - resto1;
+  base += digite1
+  // Verificando segundo valor
+  let sum2 = 0
+  for (let i = 0; i < 10; i++) {
+    sum2 += base[i] * (11 - i)
+  }
+  const resto2 = sum2 % 11
+  let digite2 = resto2 < 2 ? 0 : 11 - resto2;
+  base += digite2
+  return cpf == base ? true : false
+}
+
+module.exports = { verificCPF }
--- static/index.js
+++ static/index.js
@@ -1,9 +1,8 @@
+const { verificCPF } = require('./cpfVerific.js')
 document.getElementById("click").addEventListener("click", () => {
   var valor = document.getElementById("cpf").value
   var cpf = valor.trim().replace(/[^\d]/g, "");
   var arrayCbf = Array.from(cpf, (x) => parseInt(x))
-
-  let base = cpf.slice(0, 9);
   if (cpf.length != 11) {
     Swal.fire({
       icon: 'error',
@@ -12,28 +11,9 @@ document.getElementById("click").addEventListener("click", () => {
     })
     return 0;
   }
-  function PrimeiroValor(arrayCbf) {
-    let sum = 0
-    for (let i = 0; i < 9; i++) {
-      sum += arrayCbf[i] * (10 - i)
-    }
-    const resto = sum % 11
-    let digite1 = resto < 2 ? 0 : 11 - resto;
-    base += digite1
-  }
-  function SegundoValor(arrayCbf) {
-    let sum = 0
-    for (let i = 0; i < 10; i++) {
-      sum += base[i] * (11 - i)
-    }
-    const resto = sum % 11
-    let digite2 = resto < 2 ? 0 : 11 - resto;
-    base += digite2
-  }
-  const teste = PrimeiroValor(arrayCbf)
-  const teste1 = SegundoValor(arrayCbf)
+  const isValid = verificCPF(arrayCbf, cpf)
   let cpfFormating = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
-  if (base == cpf) {
+  if (isValid) {
     confetti({
       particleCount: 100,
       spread: 70,
